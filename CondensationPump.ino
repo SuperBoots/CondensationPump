71847c193a3beaefd9d095fe0f03c491fe024ea1
Dan Partington <34525290+SuperBoots@users.noreply.github.com>
2022-08-05 02:04:34 -0700

    Update CondensationPump.ino
    adding pin definitions

--- CondensationPump.ino
+++ CondensationPump.ino
@@ -1,13 +1,25 @@
 // Adafruit ItsyBitsy 32u4 - 5V 16MHz
 
+const int bottomFloatSwitchPin = 0;
+const int topFloatSwitchPin = 1;
+const int redSwitchPin = 2;
+const int greenSwitchPin = 3;
+const int greenLEDPin = 5;
+const int redLEDPin = 7;
+const int 120vRelayPin = 9;
+const int BuiltInLED = 9;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(0, INPUT); // floatSwitch0
-  pinMode(1, INPUT); // floatSwitch1
-  pinMode(2, OUTPUT); // Red Button LED
-  pinMode(3, OUTPUT); // 120v relay
-  pinMode(13, OUTPUT); // onboard LED
+  pinMode(bottomFloatSwitchPin, INPUT); // reads input from lower float switch
+  pinMode(topFloatSwitchPin, INPUT); // reads input from upper float switch
+  pinMode(redSwitchPin, INPUT); // reads input from red stop button
+  pinMode(greenSwitchPin, INPUT); // reads input from green manual-on button
+  pinMode(greenLEDPin, OUTPUT); // controls green ring LED around button
+  pinMode(redLEDPin, OUTPUT); // controls red ring LED around button
+  pinMode(120vRelayPin, OUTPUT); // controls 120v relay for pump control, High = on
+  pinMode(BuiltInLED, OUTPUT); // onboard LED
 }
 
 // the loop function runs over and over again forever
