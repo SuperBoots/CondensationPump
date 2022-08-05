6e2fe0ca713c21545f11fc3932491e48065c5385
Dan Partington <34525290+SuperBoots@users.noreply.github.com>
2022-08-05 01:41:31 -0700

    adding images

--- CondensationPump.ino
+++ CondensationPump.ino
@@ -3,21 +3,34 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(0, INPUT); // floatSwitch0
+  pinMode(1, INPUT); // floatSwitch1
+  pinMode(2, OUTPUT); // Red Button LED
+  pinMode(3, OUTPUT); // 120v relay
+  pinMode(13, OUTPUT); // onboard LED
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);              // wait for a second
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);              // wait for a second
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);              // wait for a second
+  int floatSwitch0 = digitalRead(0);
+  int floatSwitch1 = digitalRead(1);
+  delay(100);
+  if (floatSwitch0 != 0)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+  if (floatSwitch1 != 0)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+  }
+  else
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+  }
 }
